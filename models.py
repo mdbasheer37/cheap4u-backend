d9199c448f63dd37f3574ffc4ea8a47bb26b3b10
mdbasheer37 <muhammadibrahim3766@gmail.com>
2026-04-15 23:45:38 +0100

    Update models.py

--- models.py
+++ models.py
@@ -6,8 +6,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
-    
-    referral_code = db.Column(db.String(20), unique=True) 
+
+    # models.py User class addition
+    referral_bonus_claimed = db.Column(db.Boolean, default=False)
+    #referral_code = db.Column(db.String(20), unique=True) 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
